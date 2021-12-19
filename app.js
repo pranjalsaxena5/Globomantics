47bdc80fe8dcf02a275715583ae023e739e483d1
Pranjal <pranjalsaxena5@gmail.com>
2021-12-19 16:36:58 +0530

    Added static HTML to work with

--- app.js
+++ app.js
@@ -1,14 +1,24 @@
 const express = require('express');
-
+// const chalk = require('chalk');
+const debug = require('debug')('app');
+const morgan = require('morgan');
 const app = express();
+const path = require('path')
 
+app.use(morgan('tiny'));
+app.use(express.static(path.join(__dirname, '/public/')));
+
+
+app.set('views', './src/views');
+app.set('view engine', 'ejs');
 
 
 app.get('/', (req, res) => {
-    res.send('Hello from my app');
-})
+    res.render('index', { title: 'Welcome to Globomantics', data: ['a', 'b', 'c']} );
+});
 
 
 app.listen(3000, () => {
-    console.log("Listening on port 3000");
+    debug("Listening on port 3000");
+
 });
